bdaca3a1fd2523e0adf4379b27fbfe0a918e2019
EC2 Default User <ec2-user@ip-172-31-14-208.ec2.internal>
2022-04-12 10:45:24 +0000

    [エラ-user#index]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   #end
   
   def index
+    @user = current_user
     @book = Book.new
     @books = Book.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
  
-  resources :users, only:[:show, :edit]
   #get 'users/show'
   #get 'users/edit'
   resources :books, only: [:create, :index, :show, :destroy]
@@ -11,5 +10,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get '/homes/about' => "homes#about", as: "about"
-
+  resources :users, only:[:index, :show, :edit, :update]
 end
