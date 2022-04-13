d16b7f5a5f3ddbb4fd0252f2aeba30155edf3a55
EC2 Default User <ec2-user@ip-172-31-14-208.ec2.internal>
2022-04-13 04:16:38 +0000

    [Update]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
+  end
+  
+  def create
+    @book = User.books.new
+    user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -16,8 +29,12 @@ class UsersController < ApplicationController
   
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
- 
+
   #get 'users/show'
   #get 'users/edit'
-  resources :books, only: [:create, :index, :show, :destroy]
+  resources :books, only: [:create, :index, :edit, :update, :show, :destroy]
   # get 'books/new'
   # get 'books/index'
   # get 'books/show'
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   get '/homes/about' => "homes#about", as: "about"
-  resources :users, only:[:index, :show, :edit, :update]
+  resources :users, only:[:index, :show, :create, :edit, :update]
 end
