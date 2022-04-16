25bf3957ff7cb03e9a819167ce192820a9e93462
EC2 Default User <ec2-user@ip-172-31-14-208.ec2.internal>
2022-04-16 11:35:39 +0000

    [update]4-25

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
-  validates :name, presence: true, uniqueness: true, length: {minimum:2, maximam:20}
+  validates :name, presence: true, uniqueness: true, length: {in:2..20}
   validates :introduction,length:{maximum:50}
 
 end
