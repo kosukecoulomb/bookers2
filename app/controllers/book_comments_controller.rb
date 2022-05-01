188bee98b11e900db87575ebff1a9866433fdc9e
EC2 Default User <ec2-user@ip-172-31-14-208.ec2.internal>
2022-05-01 11:25:32 +0000

    コメント機能非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,14 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      render :book_comments
-    else
-      render 'books/show'
-    end
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
   
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    render :book_comments
   end
   
   def book_comment_params
