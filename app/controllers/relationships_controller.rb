b8e79fe01035b21149b05a5e08a80cb61e772f2d
EC2 Default User <ec2-user@ip-172-31-14-208.ec2.internal>
2022-05-05 13:48:04 +0000

    フォロー機能非同期化途中

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,21 @@
 class RelationshipsController < ApplicationController
   def create
-    @other_user = User.find(params[:follower])
-    current_user.follow(@other_user)
+    @user = User.find(params[:user_id])
+    current_user.follow(params[:user_id])
   end
 
   def destroy
-    @user = current_user.relationships.find(params[:id]).follower
+    @user = User.find(params[:user_id])
     current_user.unfollow(params[:user_id])
   end
 
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers
   end
 end
