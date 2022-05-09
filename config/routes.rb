2b7d81772231eaf7290d500b924cc48d213c6bf6
kosukecoulomb <jobch.coulomb@gmail.com>
2022-05-09 02:56:44 +0000

    [検索機能追加]

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => "relationships#followers", as: 'followers'
   end
+  get "search" => "searches#search"
 end
